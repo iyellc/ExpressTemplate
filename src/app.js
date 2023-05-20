bbd6572c6a509f681ce4fa0f891be691829dba63
Yigit Gulay <80751083+yigdo@users.noreply.github.com>
2023-05-20 21:48:34 +0300

    Add error handling

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
+const path = require('path');
 const routes = require("./routes");
-const handlebars = require('express-handlebars');
+const exphbs = require('express-handlebars');
 const { accessibleRecordsPlugin, accessibleFieldsPlugin } = require('@casl/mongoose');
 require("dotenv").config();
 
@@ -16,8 +17,33 @@ const { mongoose } = require("mongoose");
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.set('view engine', 'handlebars');
+app.engine('.hbs', exphbs.engine({
+    defaultLayout: 'main',
+    extname: '.hbs',
+    layoutsDir: path.join(__dirname, 'views/layouts')
+}));
+
+app.set('view engine', '.hbs');
 app.use("", routes)
+
+app.use("*", function (req, res, next) {
+    next(res.status(404).json({
+        success: false,
+        errorId: "PAGE_NOT_FOUND",
+        error: "Material is not found!"
+    }));
+});
+
+app.use(function (err, req, res, next) {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    res.status(err.status || 500).json({
+        success: false,
+        errorId: "UNKNOWN_ERROR_OCCURED",
+        error: (app.get("env") == "development") ? "An unknown error has occured: " + err.message : "An unknown error has occured!"
+    })
+});
 
 app.listen(PORT, async () => {
     console.log("App is online at port: " + PORT)
